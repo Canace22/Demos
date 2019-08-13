8e6357e505eaadf16c9079d726022e4074bff324
Canace <canace22@qq.com>
2019-08-13 15:26:50 +0800

    update config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
+const path = require('path');
+
+const resolve = dir => path.join(__dirname, dir);
+
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/demos/' : '/',
+  // 基本路径
+  publicPath: '/',
+  // 生产环境是否生成 sourceMap 文件
+  productionSourceMap: false,
+  // 服务器端口号
+  devServer: {
+    port: 80,
+  },
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set('@$', resolve('src'))
+      .set('components', resolve('components'))
+      .set('assets', resolve('assets'));
+  },
 };
