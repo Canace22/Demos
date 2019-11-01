47f5a929066b8aa2d7dd7cdea3b9dae3cd78eb1f
canace <canace22@qq.com>
2019-11-01 18:02:25 +0800

    add poly animation

--- src/components/animate/Battary.vue
+++ src/components/animate/Battary.vue
@@ -1,36 +1,36 @@
 <template>
-  <div class="battery-log"></div>
+	<div class="battery-log"></div>
 </template>
 
 <script>
-import anime from 'animejs';
+	import anime from "animejs";
 
-export default {
-  mounted() {
-    const el = document.querySelector('.battery-log');
-    this.makeBattary(el);
-  },
-  methods: {
-    makeBattary(el) {
-      const logEl = el;
-      const battery = {
-        charged: '0%',
-        cycles: 120,
-      };
+	export default {
+		mounted() {
+			const el = document.querySelector(".battery-log");
+			this.makeBattary(el);
+		},
+		methods: {
+			makeBattary(el) {
+				const logEl = el;
+				const battery = {
+					charged: "0%",
+					cycles: 120
+				};
 
-      anime({
-        targets: battery,
-        charged: '100%',
-        cycles: 130,
-
-        easing: 'linear',
-        update() {
-          logEl.innerHTML = JSON.stringify(battery.cycles);
-        },
-      });
-    },
-  },
-};
+				anime({
+					targets: battery,
+					charged: "100%",
+					round: 1,
+					cycles: 130,
+					easing: "linear",
+					update() {
+						logEl.innerHTML = JSON.stringify(battery.cycles);
+					}
+				});
+			}
+		}
+	};
 </script>
 
 <style lang="scss" scoped>
--- src/components/animate/Easing.vue
+++ src/components/animate/Easing.vue
@@ -1,167 +1,134 @@
 <template>
-  <div class="animation-wrapper">
-    <div class="feature-animation">
-      <div class="easing-visualizer">
-        <div class="wrapper bars-wrapper"></div>
-        <div class="wrapper dots-wrapper"></div>
-      </div>
-    </div>
-  </div>
+	<div class="stagger-visualizer">
+		<svg class="morphing-demo">
+			<polyline class="polymorph" fill="none" stroke="#fff" stroke-width="5" />
+		</svg>
+	</div>
 </template>
 
 <script>
-import anime from 'animejs';
+	import anime from "animejs/lib/anime.es.js";
 
-export default {
-  mounted() {
-    // this.easingsAnimation();
-  },
-  methods: {
-    fitElementToParent(el, padding) {
-      let timeout = null;
-      const resize = () => {
-        if (timeout) clearTimeout(timeout);
-        anime.set(el, { scale: 1 });
-        const pad = padding || 0;
-        const parentEl = el.parentNode;
-        const elOffsetWidth = el.offsetWidth - pad;
-        const parentOffsetWidth = parentEl.offsetWidth;
-        const ratio = parentOffsetWidth / elOffsetWidth;
-        timeout = setTimeout(anime.set(el, { scale: ratio }), 10);
-      };
-      resize();
-      window.addEventListener('resize', resize);
-    },
+	export default {
+		mounted() {
+			const staggerVisualizerEl = document.querySelector(".stagger-visualizer");
+			const fragment = document.createDocumentFragment();
+			const numberOfElements = 100;
 
-    easingsAnimation() {
-      const easingVisualizerEl = document.querySelector('.easing-visualizer');
-      const barsWrapperEl = easingVisualizerEl.querySelector('.bars-wrapper');
-      const dotsWrapperEl = easingVisualizerEl.querySelector('.dots-wrapper');
-      const barsFragment = document.createDocumentFragment();
-      const dotsFragment = document.createDocumentFragment();
-      const numberOfBars = 91;
-      const duration = 450;
-      let animation;
+			anime({
+				targets: ".morphing-demo .polymorph",
+				points: [
+					{
+						value: [
+							"70 24 219.574 60.369 200.145 117.631 150.855 101.631 103.426 254.369",
+							"70 41 118.574 59.369 111.145 132.631 60.855 84.631 20.426 60.369"
+						]
+					},
+					// {
+					// 	value:
+					// 		"70 6 119.574 60.369 100.145 117.631 39.855 117.631 0 0"
+					// },
+					{
+						value: "0 0 100 0 200 0 300 0 400 0"
+					}
+					// {
+					// 	value:
+					// 		"70 57 136.574 54.369 89.145 100.631 28.855 132.631 38.426 64.369"
+					// },
+					// {
+					// 	value:
+					// 		"70 24 119.574 60.369 100.145 117.631 50.855 101.631 3.426 54.369"
+					// }
+				],
+				easing: "easeOutQuad",
+				duration: 2000,
+				loop: true
+			});
 
-      this.fitElementToParent(easingVisualizerEl, 0);
+			// for (let i = 0; i < numberOfElements; i++) {
+			// 	fragment.appendChild(document.createElement("div"));
+			// }
 
-      for (let i = 0; i < numberOfBars; i++) {
-        const barEl = document.createElement('div');
-        const dotEl = document.createElement('div');
-        barEl.classList.add('bar');
-        dotEl.classList.add('dot');
-        dotEl.classList.add('color-red');
-        barsFragment.appendChild(barEl);
-        dotsFragment.appendChild(dotEl);
-      }
+			// staggerVisualizerEl.appendChild(fragment);
 
-      barsWrapperEl.appendChild(barsFragment);
-      dotsWrapperEl.appendChild(dotsFragment);
+			// const staggersAnimation = anime
+			// 	.timeline({
+			// 		targets: ".stagger-visualizer div",
+			// 		easing: "easeInOutSine",
+			// 		// delay: anime.stagger(50),
+			// 		// loop: true,
+			// 		// autoplay: false,
+			// 		duration: 600,
+			// 		loopComplete: a => console.log("end")
+			// 	})
+			// 	.add({
+			// 		scale: anime.stagger([1.5, 1], {
+			// 			from: "center",
+			// 			easing: "easeInOutCirc"
+			// 		}),
+			// 		translateX: anime.stagger([0, 100]),
+			// 		translateY: anime.stagger([-50, 100]),
+			// 		rotate: 0,
+			// 		delay: 0
+			// 		// delay: anime.stagger(1, { from: "center" })
+			// 	})
+			// 	.add({
+			// 		scale: anime.stagger([1.5, 1], {
+			// 			from: "center",
+			// 			easing: "easeInOutCirc"
+			// 		}),
+			// 		translateX: anime.stagger([100, 0]),
+			// 		translateY: anime.stagger([100, -10]),
+			// 		rotate: 0,
+			// 		delay: 0
+			// 		// delay: anime.stagger(1, { from: "first" })
+			// 	})
+			// 	// .add({
+			// 	// 	scale: anime.stagger([1.5, 1], {
+			// 	// 		from: "center",
+			// 	// 		easing: "easeInOutCirc"
+			// 	// 	}),
+			// 	// 	translateX: anime.stagger([100, -100]),
+			// 	// 	translateY: anime.stagger([50, 0]),
+			// 	// 	rotate: 0,
+			// 	// 	delay: anime.stagger(1, { from: "first" })
+			// 	// })
+			// 	.add({
+			// 		translateX: 0,
+			// 		translateY: 0,
+			// 		scale: 1,
+			// 		rotate: 360,
+			// 		duration: 1000,
+			// 		delay: 0
+			// 	});
 
-      const play = () => {
-        const easings = [];
-        const eases = Object.values(anime.penner);
-        console.log(eases);
-        eases.forEach((ease, index) => {
-          easings.push(ease);
-          easings.push(`steps(${anime.random(5, 20)})`);
-          easings.push(`steps(${anime.random(5, 20)})`);
-          easings.push('cubicBezier(0.545, 0.475, 0.145, 1)');
-          eases[index] = easings[anime.random(0, easings.length - 1)];
-          animation = anime
-            .timeline({
-              duration,
-              easing: ease,
-              complete: play,
-            })
-            .add({
-              targets: '.easing-visualizer .bar',
-              scaleY: anime.stagger([1, 44], {
-                easing: ease,
-                from: 'center',
-                direction: 'reverse',
-              }),
-              delay: anime.stagger(7, { from: 'center' }),
-            })
-            .add(
-              {
-                targets: '.easing-visualizer .dot',
-                translateY: anime.stagger(['-160px', '160px'], {
-                  easing: ease,
-                  from: 'last',
-                }),
-                delay: anime.stagger(7, { from: 'center' }),
-              },
-              0,
-            );
-        });
-      };
-      play();
-    },
-  },
-};
+			// staggersAnimation.play();
+		},
+		methods: {}
+	};
 </script>
 
 <style lang="scss" scoped>
-body {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: absolute;
-  width: 100%;
-  height: 100vh;
-}
+	.morphing-demo {
+		width: 500px;
+		height: 500px;
+	}
+</style>
+<style>
+	.stagger-visualizer {
+		display: flex;
+		/*flex-wrap: wrap;*/
+		justify-content: center;
+		align-items: center;
+		/*   width: 450px;
+																																					  height: 450px; */
+	}
 
-.animation-wrapper {
-  width: 100%;
-  max-width: 910px;
-  margin-left: 32px;
-  margin-right: 32px;
-}
-
-.easing-visualizer {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  width: 910px;
-  height: 455px;
-  margin: -227px 0 0 -455px;
-  .wrapper {
-    position: absolute;
-    bottom: 0;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    width: 100%;
-    height: 100%;
-  }
-  .bar {
-    width: 10px;
-    height: 10px;
-    margin: 0;
-    background-image: linear-gradient(
-      180deg,
-      #373734 0%,
-      #242423 52%,
-      #0d0d0c 100%
-    );
-    transform-origin: 50% 50%;
-  }
-  .dot {
-    position: relative;
-    width: 10px;
-    height: 10px;
-    margin: 0;
-    background-color: currentColor;
-    border-radius: 50%;
-  }
-}
-
-.bars-wrapper,
-.dots-wrapper {
-  transform: translateZ(0);
-}
+	.stagger-visualizer div {
+		/*position: absolute;*/
+		width: 5px;
+		height: 5px;
+		border: 1px solid #fff;
+		background-color: #fff;
+	}
 </style>
